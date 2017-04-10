8cb2d182cdd0166dd81f7eabbd640c97bdd240ae
Christian <christian@styla.com>
2017-04-10 11:56:19 +0200

    new Deck model

--- app/Deck.php
+++ app/Deck.php
@@ -2,9 +2,19 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class Deck extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description', 'decklist', 'owner_id'
+    ];
 }
