fcb305088a7e6c5d838c33307794e7c19b842c42
Christian <christian@styla.com>
2017-05-15 17:50:14 +0200

    deckrow colors

--- resources/views/partials/deckrow.blade.php
+++ resources/views/partials/deckrow.blade.php
@@ -1,10 +1,8 @@
 <tr>
     <td>
-        <span class="ms ms-cost ms-r"></span>
-        <span class="ms ms-cost ms-w"></span>
-        <span class="ms ms-cost ms-g"></span>
-        <span class="ms ms-cost ms-u"></span>
-        <span class="ms ms-cost ms-b"></span>
+        @foreach( json_decode($deck->colors) as $color )
+            <span class="ms ms-cost ms-{{ strtolower( $color ) }}"></span>
+        @endforeach
     </td>
     <th>{{ $deck->title }}</th>
     <td>{{ $deck->format }}</td>
@@ -12,7 +10,7 @@
     @if(isset( $deck->owner_id ))
         {{ $deck->owner_id }}
     @else
-        Anonymous {{ $deck->link }}
+        Anonymous
     @endif
     </td>
     <td class="has-text-right"><a href="{{ $deck->link }}" class="button is-primary is-small">View <i class="fa fa-fw fa-chevron-right"></i></a></td>    
