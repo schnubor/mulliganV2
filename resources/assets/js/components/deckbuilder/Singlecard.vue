b1b9929affe58c19ac7dbfd3f0021828a97ef2fb
schnubor <info@chko.org>
2017-09-08 00:06:57 +0200

    added box shadow to cards

--- resources/assets/js/components/deckbuilder/Singlecard.vue
+++ resources/assets/js/components/deckbuilder/Singlecard.vue
@@ -124,7 +124,7 @@
 
         &:hover {
             transform: translateY(-5px);
-            box-shadow: 0 5px 16px 2px rgba(0,0,0,.4);
+            box-shadow: 0 5px 20px rgba(0,0,0,.4);
         }
 
         & > .cardcontent {
