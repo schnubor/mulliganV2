0ec7d32f1f5fec4e61522754f271f6b965314aa5
Christian Kornderfer <christian@perspective.co>
2017-10-14 00:48:39 +0200

    logout/login flow complete

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,7 +1,7 @@
-<header class="nav">
+<header class="navbar is-transparent">
     <div class="container">
-        <div class="nav-left">
-            <a class="nav-item" href="/">
+        <div class="navbar-start">
+            <a class="navbar-item" href="/">
                 <h1><strong>Mulligan</strong></h1>
             </a>
         </div>
@@ -10,20 +10,36 @@
             <span></span>
             <span></span>
         </span>
-        <div class="nav-right nav-menu">
-            <a href="{{ route('home') }}" class="nav-item {{ strpos(Route::currentRouteName(), 'home') === 0 ? 'is-active' : '' }}">
+        <div class="navbar-end nav-menu">
+            <a href="{{ route('home') }}" class="navbar-item {{ strpos(Route::currentRouteName(), 'home') === 0 ? 'is-active' : '' }}">
                 Home
             </a>
-            <a href="{{ route('deckbuilder') }}" class="nav-item {{ strpos(Route::currentRouteName(), 'deckbuilder') === 0 ? 'is-active' : '' }}">
+            <a href="{{ route('deckbuilder') }}" class="navbar-item {{ strpos(Route::currentRouteName(), 'deckbuilder') === 0 ? 'is-active' : '' }}">
                 Deckbuilder
             </a>
-            <span class="nav-item">
-                <a class="button is-primary">
-                    <span class="icon">
-                        <i class="fa fa-sign-in"></i>
-                    </span>
-                    <span>Login</span>
-                </a>
+            <span class="navbar-item">
+                @if( Auth::check() )
+                    <a class="button is-primary" href="<?php echo e(route('logout')); ?>"
+                        onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                        <span class="icon">
+                            <i class="fa fa-sign-out"></i>
+                        </span>
+                        <span>Logout</span>
+                    </a>
+
+                    <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" style="display: none;">
+                        <?php echo e(csrf_field()); ?>
+
+                    </form>
+                @else
+                    <a class="button is-primary" href="{{ route('login') }}">
+                        <span class="icon">
+                            <i class="fa fa-sign-in"></i>
+                        </span>
+                        <span>Login</span>
+                    </a>
+                @endif
             </span>
         </div>
     </div>
