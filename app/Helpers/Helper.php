ce6209f69d0416501982e09c8af0f039cfd668a6
Christian Korndoerfer <christian@perspective.co>
2018-01-13 16:07:43 +0100

    fixed split mana for single card

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -18,6 +18,21 @@ class Helper
         $pattern[9] = '/{E}/';
         $pattern[10] = '/{(\d)}/';
         $pattern[11] = '/{X}/';
+        $pattern[12] = '/{W\/U}/';
+        $pattern[13] = '/{U\/B}/';
+        $pattern[14] = '/{B\/R}/';
+        $pattern[15] = '/{R\/G}/';
+        $pattern[16] = '/{G\/W}/';
+        $pattern[17] = '/{W\/B}/';
+        $pattern[18] = '/{U\/R}/';
+        $pattern[19] = '/{B\/G}/';
+        $pattern[20] = '/{R\/W}/';
+        $pattern[21] = '/{G\/U}/';
+        $pattern[22] = '/{2\/W}/';
+        $pattern[23] = '/{2\/U}/';
+        $pattern[24] = '/{2\/B}/';
+        $pattern[25] = '/{2\/R}/';
+        $pattern[26] = '/{2\/G}/';
 
         $replacement = array();
         $replacement[0] = '<i class="ms ms-tap ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
@@ -32,6 +47,21 @@ class Helper
         $replacement[9] = '<i class="ms ms-e ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
         $replacement[10] = '<i class="ms ms-\1 ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
         $replacement[11] = '<i class="ms ms-x ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[12] = '<i class="ms ms-split ms-wu ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[13] = '<i class="ms ms-split ms-ub ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[14] = '<i class="ms ms-split ms-br ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[15] = '<i class="ms ms-split ms-rg ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[16] = '<i class="ms ms-split ms-gw ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[17] = '<i class="ms ms-split ms-wb ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[18] = '<i class="ms ms-split ms-ur ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[19] = '<i class="ms ms-split ms-bg ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[20] = '<i class="ms ms-split ms-rw ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[21] = '<i class="ms ms-split ms-gu ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[22] = '<i class="ms ms-split ms-2w ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[23] = '<i class="ms ms-split ms-2u ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[24] = '<i class="ms ms-split ms-2b ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[25] = '<i class="ms ms-split ms-2r ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
+        $replacement[26] = '<i class="ms ms-split ms-2g ms-cost ' . ($big ? 'ms-2x ' : '') . ($shadow ? 'ms-shadow ' : '') . '"></i> ';
 
         $newText = preg_replace( $pattern, $replacement, $text );
 
